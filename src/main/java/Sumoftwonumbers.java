645c1f81feea55d2a42d2242b4bebcfdb453bae0
Pavan <pavankumar1597@gmail.com>
2022-02-13 12:25:48 +0530

    leetcode

--- src/main/java/Sumoftwonumbers.java
+++ src/main/java/Sumoftwonumbers.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Sumoftwonumbers {
 
@@ -19,4 +21,30 @@ public class Sumoftwonumbers {
 
 
         }
+
+
+    public static int[] twoNumberSum(int[] array, int targetSum) {
+
+        Arrays.sort(array);
+        int l = 0;
+        int r= array.length-1;
+
+        while(l<r){
+            int sum =array[l]+array[r] ;
+            if(sum==targetSum){
+                return new int[]{array[l],array[r]};
+            }else if (sum<targetSum){
+                l++;
+
+            }else if (sum>targetSum)
+                r--;
+
+        }
+        return new int[]{};
+
     }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoNumberSum(new int[] {4, 6, 1}, 5)));
+    }
+}
---
+++ src/main/java/validateSubsequence.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Program {
+    public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
+        // Write your code here.
+        int i = 0;
+        int j = 0;
+        while(i<array.size() && j<sequence.size()){
+            if(array.get(i)==sequence.get(j)){
+                i++;
+                j++;
+            }else{
+                i++;
+            }
+        }
+        return j>=sequence.size();
+    }
+
+
+    public static void main(String[] args) {
+        var array = Arrays.asList(5, 1, 22, 25, 6, -1, 8, 10);
+        var sequence = Arrays.asList(1, 6, -1, 10);
+    Program.isValidSubsequence(array, sequence);
+    }
+}
